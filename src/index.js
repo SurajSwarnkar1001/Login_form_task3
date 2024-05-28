86675ee3f5cc51731849afe20130944bff359919
Suraj Swarnkar <SurajSwarnkar1001@gmail.com>
2024-05-28 10:25:45 +0530

    nodejs Project first commit

---
+++ src/index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const path = require("path");
+const collection = require("./config");
+//hashing the password or store password it has slow runtime
+const bcrypt = require('bcrypt');
+const { error } = require("console");
+
+const app = express();
+// JSON format convert karta hai 
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+    // res.render("signup");
+    res.render("error1");
+});
+
+// Register User
+app.post("/signup", async (req, res) => {
+
+    const data = {
+        name: req.body.username,
+        mail:req.body.email,
+        password: req.body.password
+    }
+
+    // Check username database ma hai ki nahi
+    const existingUser = await collection.findOne({ name: data.name });
+
+    if (existingUser) {
+        res.send('User already exists. Please choose a different username.');
+    } else {
+        const saltRounds = 10; 
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
+        data.password = hashedPassword; 
+
+        const userdata = await collection.insertMany(data);
+        console.log(userdata);
+    }
+
+});
+
+// Login user 
+app.post("/login", async (req, res) => {
+    try {
+        const check = await collection.findOne({ name: req.body.username });
+        if (!check) {
+            res.render("usernotfounf");
+        }
+        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+        if (!isPasswordMatch) {
+            res.render("error1")
+        }
+        else {
+            res.render("home");
+        }
+    }
+    catch {
+        res.render("wrongDetails");
+    }
+});
+
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
